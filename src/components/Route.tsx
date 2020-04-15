064968f52b5900079892e0560a23e46e794fa93a
Vladimir Josan <vladimir.josan@ebs-integrator.com>
2020-04-15 22:48:08 +0300

    removed any in types and added render callback in Route component

--- src/components/Route.tsx
+++ src/components/Route.tsx
@@ -1,18 +1,19 @@
 import * as React from 'react';
-import { Route as OldRoute, RouteProps as OldRouteProps } from 'react-router-dom';
+import { Route as OldRoute, RouteProps as OldRouteProps, RouteComponentProps } from 'react-router-dom';
 import { Routes } from './CreateRouter';
 
 interface RouteProps extends Pick<Routes, Exclude<keyof Routes, 'name'>> {
   renderSuspense?: React.FC<any>;
   component: React.FC<OldRouteProps>;
+  render?: (routeProps: RouteComponentProps) => JSX.Element;
 }
 
-export const Route: React.FC<RouteProps> = ({ renderSuspense, suspense, component: Component, ...route }) => (
+export const Route: React.FC<RouteProps> = ({ renderSuspense, suspense, component: Component, render, ...route }) => (
   <OldRoute
     {...route}
-    render={(routeProps): React.ReactNode => (
+    render={(routeProps): JSX.Element => (
       <React.Suspense fallback={renderSuspense || <span>Loading ...</span>}>
-        <Component {...routeProps} />
+        {typeof render === 'function' ? render(routeProps) : <Component {...routeProps} />}
       </React.Suspense>
     )}
   />
--- src/components/RoutesGroup.tsx
+++ src/components/RoutesGroup.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { BrowserRouter, Router } from 'react-router-dom';
+import { History } from 'history';
 
-export const RoutesGroup: React.FC<{ history?: any }> = ({ history, children }) =>
+export const RoutesGroup: React.FC<{ history?: History<History.PoorMansUnknown> }> = ({ history, children }) =>
   history ? <Router history={history}>{children}</Router> : <BrowserRouter>{children}</BrowserRouter>;
 
 export default {};
